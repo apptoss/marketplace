d6206a7182392a2083054234c80d7a1a25b5e1ad
network <net@d2.plus>
2024-08-09 08:41:20 +0700

    move scripts

--- scripts/gen_abi.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-Node_API=http://127.0.0.1:8080
-
-CONTRACT_ADDRESS=$(cat ./move/contract_address.txt)
-
-PACKAGE_NAME=$(cat ./move/sources/$(ls ./move/sources/ | head -n 1) | head -n 1 | sed -n 's/module [^:]*::\(.*\) {/\1/p')
-
-echo "export const ABI = $(curl $Node_API/v1/accounts/$CONTRACT_ADDRESS/module/$PACKAGE_NAME | sed -n 's/.*"abi":\({.*}\).*}$/\1/p') as const" > ./dapp/aptos/$PACKAGE_NAME-abi.ts
