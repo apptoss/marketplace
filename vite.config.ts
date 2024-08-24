748b8d917a4a91c9d1f1018b05f967994f6c4550
network <net@d2.plus>
2024-08-24 11:42:16 +0700

    bind ip for device test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./dapp"),
     },
+  },
+  server: {
+    host: true,
   },
 })
