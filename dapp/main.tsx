1c0229587e357c678da9b3a156e68da8eb125c64
network <net@d2.plus>
2024-09-02 17:12:42 +0700

    size indicator

--- dapp/main.tsx
+++ dapp/main.tsx
@@ -1,14 +1,16 @@
+import { TailwindIndicator } from "@/components/tailwind-indicator.tsx"
 import { Toaster } from "@/components/ui/toaster.tsx"
+import { WalletProvider } from '@/components/wallet-provider.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.tsx'
-import { WalletProvider } from './components/wallet-provider.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WalletProvider>
       <App />
+      <TailwindIndicator />
       <Toaster />
     </WalletProvider>
   </React.StrictMode>,
