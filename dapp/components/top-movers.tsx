ef5d19bac7bdd38c3e66157664501cda62b867e0
network <net@d2.plus>
2024-09-23 18:16:04 +0700

    recent bets wip

--- dapp/components/top-movers.tsx
+++ dapp/components/top-movers.tsx
@@ -1,5 +1,10 @@
+import { aptos } from "@/aptos/client"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { APPTOSS_PACKAGE } from "@/constants"
+import { formatApt } from "@/lib/units"
+import { BadgeCent, Dices, Rocket } from "lucide-react"
+import { useEffect, useState } from "react"
 import { Card, CardHeader } from "./ui/card"
 
 export function TopMoverSection() {
@@ -11,7 +16,52 @@ export function TopMoverSection() {
   )
 }
 
+interface BetEvent {
+  game: 'dice' | 'coinflip' | 'limbo',
+  player: string
+  pay_ratio: string
+  transaction_version: number
+  collateral: string
+  payout: string
+}
+
 export function TopMovers() {
+  const [recentBets, setRecentBets] = useState<BetEvent[]>([])
+  
+  useEffect(() => {
+    aptos.getEvents({
+      options: {
+        where: {
+          indexed_type: {
+            _in: [
+              `${APPTOSS_PACKAGE}::dice::Dice`,
+              `${APPTOSS_PACKAGE}::limbo::Limbo`,
+              `${APPTOSS_PACKAGE}::coinflip::CoinFlip`,
+            ],
+          },
+        },
+        orderBy: [{"transaction_block_height": "desc"}],
+        limit: 20,
+      },
+    }).then((events) => {
+      console.log("events", events)
+      const data = events.map((ev) => {
+        const collateral = Number(ev.data.collateral)
+        const payout = ev.data.pay_ratio_bps > 0 ? collateral * Number(ev.data.pay_ratio_bps) / 10_000 : 0
+        return {
+          game: ev.type.split("::")[1] as 'dice' | 'coinflip' | 'limbo',
+          player: ev.data.player,
+          pay_ratio: (ev.data.pay_ratio_bps / 10_000).toString(),
+          transaction_version: ev.transaction_version,
+          collateral: formatApt(collateral),
+          payout: formatApt(payout),
+        }
+      })
+      console.log("data", data)
+      setRecentBets(data)
+    })
+  }, [])
+  
   return (
     <Tabs defaultValue="recent">
       <TabsList className="grid w-full grid-cols-2">
@@ -20,10 +70,10 @@ export function TopMovers() {
       </TabsList>
       <TabsContent value="recent">
         <Card>
-          <CardHeader>
-            <SkeletonDemo />
-            <SkeletonDemo />
-            <SkeletonDemo />
+          <CardHeader className="grid gap-8">
+            {recentBets.map((bet) => 
+              <BetItem game={bet.game} player={bet.player} collateral={bet.collateral} multiplier={bet.pay_ratio} payout={bet.payout} key={bet.transaction_version} />
+            )}
           </CardHeader>
         </Card>
       </TabsContent>
@@ -32,10 +82,35 @@ export function TopMovers() {
           <CardHeader>
             <SkeletonDemo />
             <SkeletonDemo />
+            <SkeletonDemo />
           </CardHeader>
         </Card>
       </TabsContent>
     </Tabs>
+  )
+}
+
+export interface BetItemProps {
+  game: 'dice' | 'coinflip' | 'limbo',
+  player: string,
+  collateral: string,
+  multiplier: string,
+  payout: string,
+}
+
+export function BetItem({ game, player, collateral, multiplier, payout }: BetItemProps) {
+  return (
+    <div className="flex items-center gap-4">
+      {game === 'dice' && <Dices className="h-6 w-6" />}
+      {game === 'coinflip' && <BadgeCent className="h-6 w-6" />}
+      {game === 'limbo' && <Rocket className="h-6 w-6" />}
+      
+      <div className="grid gap-1">
+        <p className="text-sm leading-none">Bet {collateral} APT {multiplier}×</p>
+        <p className="text-sm leading-none max-w-[120px] truncate">{player}</p>
+      </div>
+      <div className="ml-auto font-medium">{payout}</div>
+    </div>
   )
 }
 
--- dapp/constants.ts
+++ dapp/constants.ts
@@ -1,2 +1,3 @@
 export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet"
+export const APPTOSS_PACKAGE = import.meta.env.VITE_PACKAGE_ADDRESS
 export const ORIGIN = import.meta.env.VITE_ORIGIN_ADDRESS
