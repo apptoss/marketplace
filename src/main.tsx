f2eb87dbecfcba6a9b66370864157d721b6ce015
network <net@d2.plus>
2025-06-19 09:33:22 +0700

    Sun and Moon

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import { ModeToggle } from "@/components/mode-toggle"
+import { ThemeProvider } from "@/components/theme-provider"
 
 const rootElement = document.getElementById("root")
 if (!rootElement) throw new Error("Root element not found")
 
 createRoot(rootElement).render(
 	<StrictMode>
-		<h1>A Skin Builder Base</h1>
+		<ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+			<div className="flex items-center justify-between p-4">
+				<h1>A Skin Builder Base</h1>
+				<ModeToggle />
+			</div>
+		</ThemeProvider>
 	</StrictMode>,
 )
