3b899224855f833be6c1b53f73f817dd4b014ff8
network <net@d2.plus>
2025-06-19 07:32:49 +0700

    A Skin Builder Base

--- dapp/aptos/client.ts
+++
@@ -1,8 +0,0 @@
-import { NETWORK } from "@/constants"
-import { Aptos, AptosConfig } from "@aptos-labs/ts-sdk"
-import { createSurfClient } from '@thalalabs/surf'
-
-// Reuse same Aptos instance to utilize cookie based sticky routing
-export const aptos = new Aptos(new AptosConfig({ network: NETWORK }))
-
-export const client = createSurfClient(aptos)
--- dapp/lib/units.ts
+++
@@ -1,7 +0,0 @@
-export function formatApt(octa: number): string {
-  return (Math.floor(octa * 100 / 1e8) / 100).toString()
-}
-
-export function parseApt(apt: number): number {
-  return Math.floor(apt * 1e8)
-}
