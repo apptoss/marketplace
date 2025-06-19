43cb6708f2875c188160b0f65828f96b6cca2680
network <net@d2.plus>
2025-06-19 09:53:17 +0700

    Connect

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import { Network } from "@aptos-labs/ts-sdk"
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react"
 import { ModeToggle } from "@/components/mode-toggle"
 import { ThemeProvider } from "@/components/theme-provider"
+import { WalletSelector } from "@/components/WalletSelector"
 
 const rootElement = document.getElementById("root")
 if (!rootElement) throw new Error("Root element not found")
@@ -10,10 +13,21 @@ if (!rootElement) throw new Error("Root element not found")
 createRoot(rootElement).render(
 	<StrictMode>
 		<ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-			<div className="flex items-center justify-between p-4">
-				<h1>A Skin Builder Base</h1>
-				<ModeToggle />
-			</div>
+			<AptosWalletAdapterProvider
+				autoConnect={true}
+				dappConfig={{ network: Network.TESTNET }}
+				onError={(error) => {
+					console.log("error", error)
+				}}
+			>
+				<div className="flex items-center justify-between p-4">
+					<h1>A Skin Builder Base</h1>
+					<div className="flex items-center gap-2">
+						<WalletSelector />
+						<ModeToggle />
+					</div>
+				</div>
+			</AptosWalletAdapterProvider>
 		</ThemeProvider>
 	</StrictMode>,
 )
